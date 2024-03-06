e8bf90de738f80ffa45f6e964fc62bc0855212e9
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-06 19:29:15 +0900

    Title: 길이 단위 변환하기, Time: 222ms, Memory: 13MB, Status: Passed - CodeTree

--- 240306/길이 단위 변환하기/change-length-unit.js
+++ 240306/길이 단위 변환하기/change-length-unit.js
@@ -1,5 +1,5 @@
 const ft = 30.48
 const mi = 160934
 
-console.log(`9.2${ft} = ${(9.2*ft).toFixed(1)}cm
-1.3${mi} = ${(1.3*mi).toFixed(1)}cm`)
+console.log(`9.2ft = ${(9.2*ft).toFixed(1)}cm
+1.3mi = ${(1.3*mi).toFixed(1)}cm`)
