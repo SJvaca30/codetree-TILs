a0a5ab119bd5b2b354cf4264870e64afedccdb03
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-12 15:16:34 +0900

    Title: 합과 차의 나눗셈, Time: 212ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240312/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+let input = fs.readFileSync(0).toString().trim().split(" ")
+
+let a = Number(input[0])
+let b = Number(input[1])
+
+let c = a+b
+let d = a-b
+
+
+console.log((c/d).toFixed(2))
