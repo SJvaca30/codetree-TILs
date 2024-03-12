0967ddaccf3b9b2623dddcf00d994f2b99573b9e
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-12 10:20:11 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 218ms, Memory: 15MB, Status: Passed - CodeTree

--- 240312/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.js
+++ 240312/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
-let [a, b] = input.map((k)=>(Number(k)%2===0 ? Number(k)+8 : Number(k)*3));
+let [a, b] = input.map((k,l)=>(Number(l)%2===0 ? Number(k)+8 : Number(k)*3));
 
 console.log(`${a}
 ${b}
