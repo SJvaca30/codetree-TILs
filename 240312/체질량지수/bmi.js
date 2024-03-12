b14bdae72d00847b78bad1084b3b3a8d22502949
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-12 16:26:01 +0900

    Title: 체질량지수, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240312/체질량지수/bmi.js
+++ 240312/체질량지수/bmi.js
@@ -1,10 +1,11 @@
 // 입력 및 변수 선언
 const fs = require("fs");
 let n = fs.readFileSync(0).toString().trim().split(" ");
-let kg = Number(n[0])
-let m = Number(n[1])**2
+let kg = Number(n[1])
+let m = (Number(n[0])/100)**2
 let bmi = parseInt(kg/m)
 // 출력
+
 if (bmi>=25) {
     console.log(bmi+"\n"+"Obesity")
 } else { console.log(bmi)}
