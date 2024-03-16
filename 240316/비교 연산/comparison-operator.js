9f4a9d75835caf43801f2b30ecb9c3ed2ccfda67
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-16 22:30:20 +0900

    Title: 비교 연산, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240316/비교 연산/comparison-operator.js
@@ -0,0 +1,36 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = input[0];
+let b = input[1];
+
+if (a>=b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
+if (a>b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
+if (a<=b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
+if (a<b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
+if (a===b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
+if (a!==b) {
+    console.log(1)
+} else {
+    console.log(2)
+}
