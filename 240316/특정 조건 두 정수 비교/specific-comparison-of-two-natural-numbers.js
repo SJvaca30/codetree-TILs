7658785406718d40f7e4a5b45c1efced4bc794fa
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-16 21:48:54 +0900

    Title: 특정 조건 두 정수 비교, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

--- 240316/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
+++ 240316/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -3,5 +3,11 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 
 let a = Number(input[0])
 let b = Number(input[1])
+let aa = "";
+let bb = "";
 
-a < b ? console.log("1 0") : console.log("0 1")
+a < b ? aa = 1 : aa = 0;
+
+a === b ? bb = 1 : bb = 0;
+
+console.log(aa+" "+bb)
