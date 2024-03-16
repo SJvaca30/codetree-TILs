b9b68b384ffb7aeb0e060bfb0c108d18ac5ca20d
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-16 13:41:56 +0900

    Title: 최대 2번의 연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240316/최대 2번의 연산/up-to-2-calculations.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString().trim());
+
+
+
+if (input % 2 === 0) {
+  let input = input/2;
+}
+
+if (input % 2 !== 0) {
+  let input = (input+1)/2;
+}
+
+console.log(parseInt(input));
