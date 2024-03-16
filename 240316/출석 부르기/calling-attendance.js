7b7c18f293dc85b064df15ce1abfba0270974592
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-16 10:59:45 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240316/출석 부르기/calling-attendance.js
+++ 240316/출석 부르기/calling-attendance.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString().trim())
 
 if (input===1){
-    console.log("John")
+    console.log("John");
 } 
 else if (input===2){
-    console.log("Tom")
+    console.log("Tom");
 } 
 else if (input===3){
-    console.log("Paul")
+    console.log("Paul");
 } 
 else {
-    consoel.log("Vacancy")
+    consoel.log("Vacancy");
 }
