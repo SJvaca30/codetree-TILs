ec64d8910f4ee7769d517bb42665aab33b7fc7b0
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-16 13:30:25 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240316/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim()
+
+if (input >= 3000) {
+    console.log("book");
+}
+else if (input >1000 && input <= 3000) {
+    console.log("mask");
+}
+else if (input >500 && input <=1000') {
+    console.log("pen");
+}
+else if (input <= 500) {
+    console.log("no");
+}
