829ba682bbc3a9711937a4404e84404c29afd4e9
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-03-19 00:38:10 +0900

    Title: 세 수의 중간값, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240319/세 수의 중간값/median-of-three-numbers.js
+++ 240319/세 수의 중간값/median-of-three-numbers.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
-let a = Number(input[0]) < Number(input[1]) || Number(input[1]) < Number(input[2]) ? 1 : 0;
+let a = Number(input[0]) < Number(input[1]) && Number(input[1]) < Number(input[2]) ? 1 : 0;
 console.log(a)
