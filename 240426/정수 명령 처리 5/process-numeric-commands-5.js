9e5b456980844cb8f87609bcdb8e13f48521a1c6
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-04-26 20:25:27 +0900

    Title: 정수 명령 처리 5, Time: 375ms, Memory: 21MB, Status: Passed - CodeTree

--- 240426/정수 명령 처리 5/process-numeric-commands-5.js
+++ 240426/정수 명령 처리 5/process-numeric-commands-5.js
@@ -1,32 +1,22 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 let arr = [];
 
-rl.on('line', function(line) {
-    input.push(line);
-}).on('close', function() {
-    const n = parseInt(input[0]);
-    for(let i = 1; i <= n; i++) {
-        const command = input[i].split(' ');
-        switch(command[0]) {
-            case 'push_back':
-                arr.push(parseInt(command[1]));
-                break;
-            case 'pop_back':
-                arr.pop();
-                break;
-            case 'size':
-                console.log(arr.length);
-                break;
-            case 'get':
-                console.log(arr[parseInt(command[1])-1]);
-                break;
-        }
+const n = parseInt(input[0]);
+for(let i = 1; i <= n; i++) {
+    const command = input[i].split(' ');
+    switch(command[0]) {
+        case 'push_back':
+            arr.push(parseInt(command[1]));
+            break;
+        case 'pop_back':
+            arr.pop();
+            break;
+        case 'size':
+            console.log(arr.length);
+            break;
+        case 'get':
+            console.log(arr[parseInt(command[1])-1]);
+            break;
     }
-    process.exit();
-});
+}
