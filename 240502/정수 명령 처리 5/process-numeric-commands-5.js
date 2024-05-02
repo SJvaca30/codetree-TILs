143cd79e50c68e8c57e32688088bac09b205ca4f
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-05-02 16:32:40 +0900

    Title: 정수 명령 처리 5, Time: 398ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240502/정수 명령 처리 5/process-numeric-commands-5.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+let arr = [];
+
+const n = parseInt(input[0]);
+for(let i = 1; i <= n; i++) {
+    const command = input[i].split(' ');
+    if (command[0] === 'push_back') {
+        arr.push(parseInt(command[1]));
+    } else if (command[0] === 'pop_back') {
+        arr.pop();
+    } else if (command[0] === 'size') {
+        console.log(arr.length);
+    } else if (command[0] === 'get') {
+        console.log(arr[parseInt(command[1])-1]);
+    }
+}
