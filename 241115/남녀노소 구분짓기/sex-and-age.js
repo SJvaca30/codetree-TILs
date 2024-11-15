8da1cb215d8c52887d3bc1f81ccf0b90f7623ea6
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-15 14:18:39 +0900

    Title: 남녀노소 구분짓기, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

--- 241115/남녀노소 구분짓기/sex-and-age.js
+++ 241115/남녀노소 구분짓기/sex-and-age.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
-let input = fs
+let [sex, age] = fs
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n")
   .map(Number);
 
-
-
   // 남 = 0, 여 = 1
   // 19세이상 = 성인
-
-  
-  let sex = input[0]
-  let age = input[1]
 
   let result = (
     (sex === 0 && age >= 19 && "MAN") ||
