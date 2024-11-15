7c66a4ab7665232a869e10734b7a3dd19a1e9935
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-15 14:33:55 +0900

    Title: 윤년인가, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 241115/윤년인가/is-leap-year.js
+++ 241115/윤년인가/is-leap-year.js
@@ -6,7 +6,7 @@ let input = Number(fs
 
 
 let result = (
-    (input % 100 === 0) && (input % 400 !== 0) && "true" ||
+    (input % 100 === 0) && (input % 400 !== 0) && "false" ||
     input % 4 === 0 && "true" ||
     "false"
 )
