7f8c40f24fb6fd695e8d1c3c9071024f1e81f8aa
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-18 11:30:42 +0900

    Title: 일 수 구하기, Time: 189ms, Memory: 15MB, Status: Runtime Error - CodeTree

--- 241118/일 수 구하기/number-of-days-in-month.js
+++ 241118/일 수 구하기/number-of-days-in-month.js
@@ -1,16 +1,9 @@
 const fs = require("fs");
-let input = Number(fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim())
+const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
 
-
-
-const result = (month) => (
-  (month === 2 && 28) ||
-  ((month === 4 || month === 6 || month === 9 || month === 11) && 30) ||
-  31
+=
+const result = month => (
+  month === 2 ? 28 : ((1 << month) & 0b100111010000 ? 30 : 31)
 );
 
-
-console.log(result(input))
+console.log(result(input));
