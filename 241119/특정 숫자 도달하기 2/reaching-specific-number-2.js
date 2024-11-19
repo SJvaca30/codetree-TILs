3c261a559aa3dc6bf87ac21e81ba079ad3708f75
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-19 11:21:57 +0900

    Title: 특정 숫자 도달하기 2, Time: 175ms, Memory: 17MB, Status: Runtime Error - CodeTree

---
+++ 241119/특정 숫자 도달하기 2/reaching-specific-number-2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = Number(fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim())
+
+
+  let result = [];
+//   let i = 1;
+
+  while (input >= 1) {
+    result.push(inout)
+    input--
+  }
+
+  console.log(result.join(" "))
