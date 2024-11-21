307be1dcdeab744fac9568e9b8a895271ac2ae86
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-21 13:56:42 +0900

    Title: 청소당번 정하기, Time: 178ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241121/청소당번 정하기/cleaning-numbering.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = Number(fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim())
+
+let a = 0;
+let b = 0;
+let c = 0;
+
+let i = 1;
+while ( i <= input){
+    i%12 === 0 && c++ ||
+    i%3 === 0 && b++ ||
+    i%2 === 0 && a++;
+    i++
+}
+
+console.log(a+' '+b+' '+c)
