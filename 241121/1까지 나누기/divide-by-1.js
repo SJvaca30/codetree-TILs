245c9a1e2ffb369e6f92df0a5b8387e65d95b65d
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-21 23:26:46 +0900

    Title: 1까지 나누기, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241121/1까지 나누기/divide-by-1.js
+++ 241121/1까지 나누기/divide-by-1.js
@@ -1,17 +1,16 @@
 const fs = require("fs");
-let input= Number(fs
+let input = Number(fs
   .readFileSync("/dev/stdin")
   .toString()
   .trim())
 
-let N = input;
+let num = input; 
 let i = 1;
 
-while (true){
-  if(N/i <= 1) break;
-  N = parseInt(N/i)
-  
-  i++
+while (true) {
+  if (num / i <= 1) break;
+  num = Math.floor(num / i); 
+  i++;
 }
 
-console.log(i)
+console.log(i);
