c95086138a1438dfa4cd1df52c4835f14e09c897
SJvaca30 <122259127+SJvaca30@users.noreply.github.com>
2024-11-26 23:04:38 +0900

    Title: 3N + 1 수열, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241126/3N + 1 수열/3n-plus-1-sequence.js
+++ 241126/3N + 1 수열/3n-plus-1-sequence.js
@@ -7,10 +7,10 @@ let input= Number(fs
 let result = 0;
 
 while(result<101){
+    if (input === 1) break;
     input%2 ===0 && (input/=2) ||
     input%2 !==0 && (input=input*3+1);
     result++
-    if (input === 1) break;
 }
 
 console.log(result)
